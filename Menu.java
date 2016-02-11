9bed012b3c8397f218193ccff882a6f0f261336e
OlavOlsm <OlavOlsm@gmail.com>
2016-02-11 15:12:30 +0100

    Initial commit

---
+++ Menu.java
@@ -0,0 +1,13 @@
+package no.woact.stud.smaola14.tictactoe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
