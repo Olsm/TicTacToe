a77e1afe8cb1633c91d2febf4ee21a836ea63b35
OlavOlsm <OlavOlsm@gmail.com>
2016-02-12 17:51:36 +0100

    Disable adding elements to board after adding them, disable game and show result when game over

--- Game.java
+++ Game.java
@@ -43,6 +43,7 @@ public class Game extends AppCompatActivity {
             @Override
             public void onClick(View boardElement) {
                 addElementToBoard((TextView) boardElement);
+                boardElement.setClickable(false);
             }
         };
 
@@ -80,21 +81,25 @@ public class Game extends AppCompatActivity {
 
         // Or if there a winner in rows
         for (int i = 0; i < 7; i+= 3) {
-            if (checkForWinnerRow(i))
+            if (checkForWinnerRow(i)) {
                 winner = boardElements[i].getText().toString();
+                break;
+            }
         }
 
         // Check if there is a winner in columns
         for (int i = 0; i < 3; i++) {
-            if (checkForWinnerColumn(i))
+            if (checkForWinnerColumn(i)) {
                 winner = boardElements[i].getText().toString();
+                break;
+            }
         }
 
         // And check if there is a winner diagonally
         if (checkForWinnerDiagonal())
             winner = boardElements[4].getText().toString();
 
-        // todo: If there was a winner, end game and show result
+        // todo: save results
         if (winner.equals("X") || winner.equals("O")) {
             if (winner.equals("X"))
                 winner = playerOne;
@@ -102,24 +107,38 @@ public class Game extends AppCompatActivity {
                 winner = playerTwo;
 
             txtPlayers.setText(winner + " has won!");
+
+            for (int i = 0; i < gameBoard.getChildCount(); i++) {
+                TextView boardElement = (TextView) gameBoard.getChildAt(i);
+                boardElement.setEnabled(false);
+            }
         }
     }
 
+    // Look for a winner in a row of cells
     private boolean checkForWinnerRow (int firstCell) {
         return checkForWinnerCells(firstCell, firstCell + 1, firstCell + 2);
     }
 
+    // Look for a winner in a column of cells
     private boolean checkForWinnerColumn (int firstCell) {
         return checkForWinnerCells(firstCell, firstCell + 3, firstCell + 6);
     }
 
+    // Look for a winner diagonally of cells
     private boolean checkForWinnerDiagonal () {
         return  checkForWinnerCells(0, 4, 8)
                 || checkForWinnerCells(2, 4, 6);
     }
 
+    // Compare 3 cells to see if there is a winner
     private boolean checkForWinnerCells (int cellOne, int cellTwo, int cellThree) {
-        return boardElements[cellOne].getText().equals(boardElements[cellTwo].getText())
-                && boardElements[cellOne].getText().equals(boardElements[cellThree].getText());
+        CharSequence elementOne = boardElements[cellOne].getText();
+        CharSequence elementTwo = boardElements[cellTwo].getText();
+        CharSequence elementThree = boardElements[cellThree].getText();
+
+        return !elementOne.toString().isEmpty()
+                && elementOne.equals(elementTwo)
+                && elementOne.equals(elementThree);
     }
 }
